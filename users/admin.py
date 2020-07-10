b9eb3ee7147576c72c8ca81d11cee1d67d551f42
Adetunji Tejumade <tejumadeadetunji@gmail.com>
2020-07-10 13:41:50 +0100

    updates

--- users/admin.py
+++ users/admin.py
@@ -11,4 +11,5 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'username', 'age', 'is_staff', ] 
+    
 admin.site.register(CustomUser, CustomUserAdmin)
--- users/forms.py
+++ users/forms.py
@@ -9,11 +9,12 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age',)
 
 
+#ihopethisworks123
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', 'age',)
